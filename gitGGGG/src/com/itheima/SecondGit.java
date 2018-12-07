909b02b5f49cefff19d2e6a0d534469cf3261afd
aa <menghaunxiyouzz@126.com>
2018-12-07 10:04:35 +0800

    将本地工程提交到git

---
+++ gitGGGG/src/com/itheima/SecondGit.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+public class SecondGit {
+
+    public static void main(String[] args) {
+        System.out.println("提交提交git");
+        System.out.println("提交提交git");
+        System.out.println("提交提交git");
+        System.out.println("提交提交git");
+        System.out.println("提交提交git");
+        System.out.println("提交提交git");
+        System.out.println("提交提交git");
+        System.out.println("提交提交git");
+        System.out.println("提交提交git");
+        System.out.println("提交提交git");
+        System.out.println("提交提交git");
+    }
+}
